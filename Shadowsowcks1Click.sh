700906660857709503d746ea45027d7201c3e62c
Dovela <33590269+kuangshao11@users.noreply.github.com>
2018-06-21 18:10:02 +0800

    Update Shadowsowcks1Click.sh

--- Shadowsowcks1Click.sh
+++ Shadowsowcks1Click.sh
@@ -2,6 +2,7 @@
 [ $(id -u) != "0" ] && { echo "Error: You must be root to run this script"; exit 1; }
 install_ssr(){
 	clear
+	stty erase '^H' && read -p " mysql服务器地址:" ssserver
 	stty erase '^H' && read -p " SSR节点ID（nodeid）:" ssnode
 	stty erase '^H' && read -p " 是否仅单端口（true | false）:" switch
 	stty erase '^H' && read -p " 单端口（oneport）:" oneport
@@ -9,6 +10,7 @@ install_ssr(){
   	git clone -b master https://github.com/kuangshao11/Shadowsowcks1Click.git && mv Shadowsowcks1Click shadowsocksr && cd shadowsocksr && chmod +x setup_cymysql.sh && chmod +x ./initcfg.sh && ./setup_cymysql.sh && ./initcfg.sh
 	rm -rf Shadowsowcks1Click.sh
 	echo 'ssr安装完成'
+	sed -i -e "s/ssserver/$ssserver/g" usermysql.json
 	sed -i -e "s/ssnode/$ssnode/g" usermysql.json
 	sed -i -e "s/switch/$switch/g" user-config.json
 	sed -i -e "s/oneport/$oneport/g" user-config.json
@@ -21,7 +23,10 @@ install_ssr(){
 	systemctl disable firewalld.service
 	chkconfig iptables off
 	echo '已关闭iptables、firewalld，如有需要请自行配置。'
-	cd /root/
+}
+install_chacha20(){
+        clear
+        cd /root/
   	wget https://github.com/jedisct1/libsodium/releases/download/1.0.16/libsodium-1.0.16.tar.gz
   	tar xf libsodium-1.0.16.tar.gz && cd libsodium-1.0.16
   	./configure && make -j2 && make install
@@ -34,10 +39,16 @@ install_ssr(){
 yum -y install git
 yum -y groupinstall "Development Tools"
 clear
+echo ' 1. 安装 SSR'
+echo ' 2. 安装 chacha20'
 echo ' 注意：此脚本基于centos7编写，其他系统可能会出问题'
 echo ' 安装 SSR'
-stty erase '^H' && read -p " 请输入数字 [1-3]:" 1
-case "$num" in
-	1)
-	install_ssr
+stty erase '^H' && read -p " 请输入数字 [1-2]:" num
+ case "$num" in
+ 	1)
+ 	install_ssr
+	;;
+	2)
+	install_chacha20
+	;;
 esac
